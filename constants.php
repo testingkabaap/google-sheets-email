9b6c7c02607c002501568179fa73953e70cb8301
Testing Baap <testingkabaap@gmail.com>
2023-12-16 18:30:57 +0530

    update changes

--- constants.php
+++ constants.php
@@ -5,6 +5,19 @@ date_default_timezone_set("Asia/Kolkata");
 $root  = "http://" . $_SERVER['HTTP_HOST'];
 $root .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
 
+defined('BRAND_NAME') or define('BRAND_NAME', "BRAND NAME");
+defined('FACEBOOK_LINK') or define('FACEBOOK_LINK', "BRAND NAME");
+defined('INSTAGRAM_LINK') or define('INSTAGRAM_LINK', "BRAND NAME");
+defined('WHATSAPP_LINK') or define('WHATSAPP_LINK', "BRAND NAME");
+
+
+/* EMAIL CONFIG */
+defined('FROM_EMAIL') or define('FROM_EMAIL', 'info@astrologydivine.com');
+defined('SMTP_HOST') or define('SMTP_HOST', 'smtp.hostinger.com');
+defined('SMTP_USERNAME') or define('SMTP_USERNAME', 'info@astrologydivine.com');
+defined('SMTP_PASSWORD') or define('SMTP_PASSWORD', 'DdwvVlZChMLeSkm4%');
+defined('SMTP_PORT') or define('SMTP_PORT', 465);
+
 defined('BASE_URL') or define('BASE_URL', $root);
 defined('ENVIRONMENT') or define('ENVIRONMENT', "development"); //development/ production
 
--- email.php
+++ email.php
@@ -2,8 +2,43 @@
 
 include('constants.php');
 
+$emailTemplate = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:v="urn:schemas-microsoft-com:vml">
+<head><meta content="text/html; charset=utf-8" http-equiv="Content-Type"/><meta content="width=device-width" name="viewport"/><meta content="IE=edge" http-equiv="X-UA-Compatible"/>
+	<title></title>
+</head>
+<body style="padding: 0;margin:0;">
+<div style="background-color: #f5f5f5;padding: 20px;">
+<h1 style="text-align: center;"><span style="color:#e74c3c;"><tt><strong>' . BRAND_NAME . '</strong></tt></span></h1>
+
+<div style="max-width: 600px;margin-left: auto;margin-right: auto;background-color: #ffffff;min-height: 100px;box-shadow: 0 0 5px #fe7c04;border-radius: 5px;padding: 10px;">
+<h2 style="text-align: center;"><font face="Arial, Helvetica, sans-serif">PROMOTION LINK</font></h2>
+
+<p style="text-align: center;"><font face="Arial, Helvetica, sans-serif">some text paragraph</font></p>
+
+<p style="text-align: center;"><a href="{link}" style="background-color:#fe7c04;font-weight:bold;display:block;width:fit-content;margin-left:auto;margin-right:auto;padding:10px 20px;color:#ffffff;text-decoration:none;border-radius:5px;"><span style="font-family:Arial,Helvetica,sans-serif;">Click to Proceed</span></a></p>
+
+<p style="text-align: center;"><span style="font-family:Arial,Helvetica,sans-serif;"><span style="font-size:12px;">if above button is not working copy below link and paste in to your browser<br />
+<font color="#3498db">{link}</font></span></span></p>
+</div>
+
+<div style="max-width: 600px;margin-left: auto;margin-right: auto;text-align: center;">
+<h3><span style="font-family:Comic Sans MS,cursive;">Stay In Touch</span></h3>
+
+<ul style="margin: 0;padding: 0;">
+  ' . (!empty(FACEBOOK_LINK) ? '<li style="display: inline-block;margin: 5px;"><a href="' . FACEBOOK_LINK . '"><img alt="Facebook" src="https://1.bp.blogspot.com/-AX8vg7IdS2g/X1ClPNqWe6I/AAAAAAAAdTM/ai2kKzC95Vg9uXi5HvQCka44dR56q3NvgCPcBGAYYCw/s100/facebook.png" style="width: 40px; height: 40px;" /></a></li>' : '') . '
+  ' . (!empty(INSTAGRAM_LINK) ? '<li style="display: inline-block;margin: 5px;"><a href="' . INSTAGRAM_LINK . '"><img alt="instagram" src="https://1.bp.blogspot.com/-YlYus9orhB4/X1ClO88_bgI/AAAAAAAAdTY/2T5Wq-vAIDIYCkn0g1blmD65phSUm9zwACPcBGAYYCw/s0/instagram.png" style="width: 40px; height: 40px;" /></a></li>' : '') . '
+	' . (!empty(WHATSAPP_LINK) ? '<li style="display: inline-block;margin: 5px;"><a href="' . WHATSAPP_LINK . '"><img alt="whatsapp" src="https://1.bp.blogspot.com/-RTpm0E7wurU/X1ClO5lsh3I/AAAAAAAAdTg/t7CSH5KmOEckFQi2gDJaI4r1VBRB11lMwCPcBGAYYCw/s0/whatsapp.png" style="width: 40px; height: 40px;" /></a></li>' : '') . '
+</ul>
+
+<p style="margin-bottom: 0;"><span style="font-family:Georgia,serif;">Email Sent by ' . BRAND_NAME . '</span></p>
+</div>
+</div>
+</body>
+</html>';
+
 //Load Composer's autoloader
-require __DIR__.'/phpmailer/vendor/autoload.php';
+require __DIR__ . '/phpmailer/vendor/autoload.php';
 
 //Import PHPMailer classes into the global namespace
 //These must be at the top of your script, not inside a function
@@ -16,7 +51,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     if (isset($_POST["promotion_link"])) {
       $subject = isset($_POST['subject']) ? $_POST['subject'] : "Promotion Link";
 
-      $email_body = $_POST["promotion_link"];      
+      // $email_body = $_POST["promotion_link"];
+      $email_body = str_replace("{link}", $_POST['promotion_link'], $emailTemplate);
 
       //Create an instance; passing `true` enables exceptions
       $mail = new PHPMailer(true);
@@ -25,15 +61,15 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         //Server settings
         // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'smtp.hostinger.com';                     //Set the SMTP server to send through
+        $mail->Host       = SMTP_HOST;                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'info@astrologydivine.com';                     //SMTP username
-        $mail->Password   = 'DdwvVlZChMLeSkm4%';                               //SMTP password
+        $mail->Username   = SMTP_USERNAME;                     //SMTP username
+        $mail->Password   = SMTP_PASSWORD;                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        $mail->Port       = SMTP_PORT;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
         //Recipients
-        $mail->setFrom('info@astrologydivine.com', 'Mailer');
+        $mail->setFrom(FROM_EMAIL, BRAND_NAME);
         // $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
         $mail->addAddress($_POST["to_email"]);               //Name is optional
         // $mail->addReplyTo('info@example.com', 'Information');
